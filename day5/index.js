5890706f290e088fb43f7c23864f3a25cd2be92b
Adam Alenius <adam.alenius@gmail.com>
2020-12-07 20:55:22 +0100

    clean up day5 and solve pt2

--- day5/index.js
+++ day5/index.js
@@ -1,6 +1,6 @@
 const { readFile, parseNewlineSeparated } = require('../utils/parsing')
 const { printMatrix } = require('../utils')
-const { split } = require('ramda')
+const { prop, sort } = require('ramda')
 
 function createBaseChart(noOfColumns, noOfRows) {
   const base = []
@@ -15,61 +15,72 @@ function createBaseChart(noOfColumns, noOfRows) {
   return base
 }
 
-function pt1(data) {
-  let seatIds = []
+function findCorrectPlace(lookingForRow, splitInstruction) {
+  const instruction = lookingForRow
+    ? splitInstruction.slice(0, 7)
+    : splitInstruction.slice(-3)
+  let minRow = 0
+  let max = lookingForRow ? 127 : 7
+  let correct = 0
+  instruction.forEach((instruction) => {
+    const lowerInstruction = lookingForRow ? 'F' : 'L'
+    const shouldKeepLower = instruction === lowerInstruction
+    const diff = max - minRow
+    const mid = Math.ceil(diff / 2) // find the midpoint
+    shouldKeepLower ? (max -= mid) : (minRow += mid)
 
-  data.forEach((instruction) => {
-    const splitInstruction = instruction.split('')
-
-    // find row info
-    const rowInstruction = splitInstruction.slice(0, 7)
-    let minRow = 0
-    let maxRow = 127
-    let correctRow = 0
-    rowInstruction.forEach((instruction) => {
-      const shouldKeepLower = instruction === 'F'
-      const diff = maxRow - minRow
-      const mid = Math.ceil(diff / 2) // find the midpoint
-      shouldKeepLower ? (maxRow -= mid) : (minRow += mid)
-
-      if (diff === 1) {
-        correctRow = shouldKeepLower ? minRow : maxRow
-      }
-    })
-
-    // check for column info
-    const columnInfo = splitInstruction.slice(-3)
-    let minColumn = 0
-    let maxColumn = 7
-    let correctColumn
-
-    columnInfo.forEach((instruction) => {
-      const shouldKeepLower = instruction === 'L'
-      const diff = maxColumn - minColumn
-      const mid = Math.ceil(diff / 2) // find the midpoint
-      shouldKeepLower ? (maxColumn -= mid) : (minColumn += mid)
-
-      if (diff === 1) {
-        correctColumn = shouldKeepLower ? minColumn : maxColumn
-      }
-    })
-
-    const seatId = correctRow * 8 + correctColumn
-    seatIds.push(seatId)
+    if (diff === 1) {
+      correct = shouldKeepLower ? minRow : max
+    }
   })
 
+  return correct
+}
+
+function getSeatInfo(data) {
+  const seatingInfo = []
+  data.forEach((instruction) => {
+    const splitInstruction = instruction.split('')
+    // find row info
+    const correctRow = findCorrectPlace(true, splitInstruction)
+    // check for column info
+    const correctColumn = findCorrectPlace(false, splitInstruction)
+    const seatId = correctRow * 8 + correctColumn
+    seatingInfo.push({ correctRow, correctColumn, seatId })
+  })
+
+  return seatingInfo
+}
+
+function solver(data) {
+  const seatInfo = getSeatInfo(data)
+  const seatIds = seatInfo.map(prop('seatId'))
+
+  // pt1
   let highestId = -Infinity
   seatIds.forEach((id) => {
     if (id > highestId) highestId = id
   })
+  console.log({ highestId })
 
-  return highestId
+  // pt2
+  const sortedSeatIds = sort((a, b) => a - b, seatIds)
+  let missingId
+  // the seat ids are in order so just check which one isn't consecutive
+  sortedSeatIds.forEach((id, ix) => {
+    if (ix === sortedSeatIds.length - 1) return //don't check last seat
+    const nextId = sortedSeatIds[ix + 1]
+    if (Math.abs(id - nextId) !== 1) missingId = id + 1
+  })
+
+  return { pt1: highestId, pt2: missingId }
 }
 
 async function day5() {
   const rawData = await readFile('day5/input.txt')
   const data = parseNewlineSeparated(rawData)
-  console.log({ pt1: pt1(data) })
+  const { pt1, pt2 } = solver(data)
+  console.log({ pt1 })
 }
 
 module.exports = day5
